66a229514fbf06656532a5e45dc28cfeebfb115f
mherreral <mherreral@eafit.edu.co>
2021-03-08 22:14:28 -0500

    Triangle and square finished

--- hack/Ambas.asm
+++ hack/Ambas.asm
@@ -1,98 +1,90 @@
 //l=80 square
 
-//@5	//5 registers wide
-//D=A
-//@0
-//M=D //store the lenght in RAM[0]
-//
-//(LOOPTOP)
-//
-//	@0
-//	D=M
-//	@pos
-//	M=D
-//	@SCREEN
-//	D=A
-//	@pos
-//	M=M+D
-//	
-//	@pos
-//	A=M
-//	M=-1
-//	
-//	@0
-//	D=M-1
-//	M=D
-//	
-//	@LOOPTOP
-//	D;JGE
+@5	//5 registers wide
+D=A
+@0
+M=D //store the lenght in RAM[0]
 
-//@5
-//D=A
-//@0
-//M=D
-//
-//(LOOPBOTTOM)
-//
-//	@0
-//	D=M
-//	@pos
-//	M=D
-//	@SCREEN
-//	D=A
-//	@pos
-//	M=D+M
-//	@2624
-//	D=A
-//	@pos
-//	M=M+D
-//	
-//	@pos
-//	A=M
-//	M=-1
-//	
-//	@0
-//	D=M-1
-//	M=D
-//	
-//	@LOOPBOTTOM
-//	D;JGE
-//
-//@80
-//D=A
-//@0
-//M=D
-//@i
-//M=0
-//
-//@SCREEN
-//    D=A
-//    @addr
-//    M=D
-//
-//(LEFT)
-//
-//    @i
-//    D=M
-//    @0
-//    D=D-M
-//    @END
-//    D;JGT   // if i>n goto END
-//
-//	@32
-//	D=A
-//	@addr
-//	M=D+M   //addr=addr+32
-//
-//    @addr
-//    A=M
-//    M=1    // RAM[addr]=1111111111111111
-//
-//    @i
-//    M=M+1   // i++
-//
-//    @LEFT
-//    0;JMP   //goto LOOP
+(LOOPTOP)
+
+	@0
+	D=M
+	@pos
+	M=D
+	@SCREEN
+	D=A
+	@pos
+	M=M+D
+	
+	@pos
+	A=M
+	M=-1
+	
+	@0
+	D=M-1
+	M=D
+	
+	@LOOPTOP
+	D;JGE
+
+@5
+D=A
+@0
+M=D
+
+(LOOPBOTTOM)
+
+	@0
+	D=M
+	@pos
+	M=D
+	@SCREEN
+	D=A
+	@pos
+	M=D+M
+	@2624
+	D=A
+	@pos
+	M=M+D
+	
+	@pos
+	A=M
+	M=-1
+	
+	@0
+	D=M-1
+	M=D
+	
+	@LOOPBOTTOM
+	D;JGE
+
+@80
+D=A
+@0
+M=D
+
+@SCREEN
+    D=A
+    @addr
+    M=D
+
+(LEFT)
+
+	@32
+	D=A
+	@addr
+	M=D+M   //addr=addr+32
+
+    @addr
+    A=M
+    M=1   
+
+	@0
+	D=M-1
+	M=D
+	
+	@LEFT
+	D;JGE
 
 @80
 D=A
@@ -113,13 +105,6 @@ M=M+D
 
 (RIGHT)
 
-    @i
-    D=M
-    @0
-    D=D-M
-    @END
-    D;JGT   // if i>n goto END
-
     @32
     D=A
     @addr
@@ -129,33 +114,122 @@ M=M+D
     A=M
     M=1    // RAM[addr]=1111111111111111
 
-    @i
-    M=M+1   // i++
+	@0
+	D=M-1
+	M=D
 
-    @RIGHT
-    0;JMP   //goto LOOP
+	@RIGHT
+	D;JGE
 
-//@SCREEN
-//D=A
-//@addr
-//M=D
-//
-//@31
-//D=A
-//@addr
-//D=D+M
-//M=D
-//
-//@addr
-//A=M
-//M=1
+/////////////////////////////////////////////////////////////
+@5
+D=A
+@0
+M=D
 
+(TRIANGLEBOTTOM)
 
+    @0
+    D=M
+    @pos
+    M=D
+    @SCREEN
+    D=A
+    @pos
+    M=D+M
+    @5504
+    D=A
+    @pos
+    M=M+D
 
+    @pos
+    A=M
+    M=-1
+
+    @0
+    D=M-1
+    M=D
+
+    @TRIANGLEBOTTOM
+    D;JGE
+
+@80
+D=A
+@0
+M=D
+@i
+M=0
+
+@SCREEN
+    D=A
+    @addr
+    M=D
+
+@addr
+D=M
+@2880
+D=D+A
+@addr
+M=D
+
+(TRIANGLELEFT)
+
+    @32
+    D=A
+    @addr
+    M=D+M   //addr=addr+32
+
+    @addr
+    A=M
+    M=1
+
+    @0
+    D=M-1
+    M=D
+
+    @TRIANGLELEFT
+    D;JGE
+
+@5
+D=A
+@0
+M=D
+
+@i
+M=0
+
+@SCREEN
+D=A
+@addr
+M=D
+
+@2881
+D=A
+@addr
+M=M+D
+
+(HIPO)
+	@i
+	D=M
+	@0
+	D=D-M
 	@END
+	D;JGT
+	
+	@addr
+	A=M
+	M=1
+	
+	@448
+	D=A+1
+	@addr
+	M=M+D
+	
+	@i
+	M=M+1
+	
+	@HIPO
 	0;JMP
-
-
 
 (END)
 	@END
